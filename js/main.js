518eea8819ab1651b08c1c278bc40f470050ce5f
Alexander Rowland <arowland900@gmail.com>
2019-01-25 15:02:19 -0800

    js functionality

--- js/main.js
+++ js/main.js
@@ -1,19 +1,35 @@
 /*----- constants -----*/ 
-
+const MAX_WRONG_GUESSES = 6;
+const WORDS = [
+    'DATATYPES', 'OBJECTS', 'ARRAYS', 
+    'FUNCTIONS', 'MVC', 'MOO', 'VARIABLES'
+]
 
 
 /*----- app's state (variables) -----*/ 
-
+let usedLetters, wrongGuesses, secret, guess;
 
 
 /*----- cached element references -----*/ 
-
+const letterBtns = document.querySelectorAll('#letters button');
+const hangmanImage = document.querySelector('section');
+const guessEl = document.getElementById('guess');
 
 
 /*----- event listeners -----*/ 
-
+document.getElementById('letters').addEventListener('click', handleLetterClick);
 
 
 /*----- functions -----*/
+function init() {
+    // usedLetters, wrongGuesses, secret, guess;
+    usedLetters = []
+    wrongGuesses = []
+}
 
+
+
+function handleLetterClick(evt){
+    console.log(evt.target.textContent);
+}
 
